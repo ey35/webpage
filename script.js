ca9658f66a7198196ddfc1d2a0b7951df81d1aa5
ey35 <96440556+ey35@users.noreply.github.com>
2024-05-12 14:19:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ function openTab(evt, tabName) {
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+window.addEventListener('load', function() {
+    var iframes = document.querySelectorAll('.song iframe');
+    iframes.forEach(function(iframe) {
+        iframe.onload = function() {
+            iframe.parentNode.classList.add('loaded');
+        }
+    });
+});
+
